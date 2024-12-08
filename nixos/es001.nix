2ba8b2beaa65b821e7c9eddac58d855890ad585a
Naiel <109038805+naielv@users.noreply.github.com>
2024-12-08 20:34:43 +0100

    Update es001.nix

--- nixos/es001.nix
+++ nixos/es001.nix
@@ -166,6 +166,8 @@ AddHandler cgi-script .cgi .pl'';
     openFirewall = true;
   };
 
+  services.tailscale.enable = true;
+
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
   #
