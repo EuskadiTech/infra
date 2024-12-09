781e337e69303340532d63f6bcd1cca90844da06
Naiel <109038805+naielv@users.noreply.github.com>
2024-12-09 12:45:48 +0100

    update

--- deploy-docker.sh
+++ deploy-docker.sh
@@ -4,7 +4,7 @@ cd /root/infra
 
 git pull
 
-docker stack deploy web -c docker/web.yml
-docker stack deploy webtop -c docker/webtop.yml
-docker stack deploy plex -c docker/plex.yml
-docker stack deploy cloudflared -c docker/cloudflared.yml
+docker stack deploy --detach=true web -c docker/web.yml
+docker stack deploy --detach=true webtop -c docker/webtop.yml
+docker stack deploy --detach=true plex -c docker/plex.yml
+docker stack deploy --detach=true cloudflared -c docker/cloudflared.yml
