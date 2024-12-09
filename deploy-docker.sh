518ee488da90e9f3be1cbb36a0d636127c41866f
Naiel <109038805+naielv@users.noreply.github.com>
2024-12-09 20:26:10 +0100

    Update

--- deploy-docker.sh
+++ deploy-docker.sh
@@ -10,3 +10,4 @@ docker stack deploy --detach=true plex -c docker/plex.yml
 docker stack deploy --detach=true cloudflared -c docker/cloudflared.yml
 docker stack deploy --detach=true paperless -c docker/paperless.yml
 docker stack deploy --detach=true registry -c docker/registry.yml
+docker stack deploy --detach=true runner -c docker/runner.yml
