a6ab53739edc28b6a7d152982fefb37f5b51aae3
Naiel <109038805+naielv@users.noreply.github.com>
2024-12-10 08:32:58 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+import os
+
+changed = os.popen('git diff --name-only HEAD^').read()
+
+for file in changed.split("\n"):
+  if "docker/" in file:
+    print(f">>> Updating {file}")
+    stack = file.split("/")[1].split(".")[0]
+    os.system(f"docker stack deploy --detach=true {stack} -c {file}")
