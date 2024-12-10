f5a306fb684b4aa657d4ea104f0a7878927b1dd4
Naiel <109038805+naielv@users.noreply.github.com>
2024-12-10 08:56:32 +0100

    Update deploy-docker.py

--- deploy-docker.py
+++ deploy-docker.py
@@ -4,6 +4,8 @@ changed = os.popen('git diff --name-only HEAD^').read()
 
 for file in changed.split("\n"):
   if "docker/" in file:
-    print(f">>> Updating {file}")
+    print(f">>> Updating: {file}")
     stack = file.split("/")[1].split(".")[0]
     os.system(f"docker stack deploy --detach=true {stack} -c {file}")
+  else:
+    print(f"<<< Not docker: {file}")
